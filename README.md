96feed5aaf1c9cd986902922cdb1df94ab6fe5f4
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2023-04-15 05:00:16 +0500

    update code

--- README.md
+++
@@ -1,1 +0,0 @@
-# abubakarshaikh.github.io
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
-    var windoheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
 
-    if (revealtop < windoheight - revealpoint) {
+    if (revealTop < windowHeight - revealPoint) {
       reveals[i].classList.add("active");
     } else {
       reveals[i].classList.remove("active");
@@ -18,29 +18,28 @@ function reveal() {
   }
 }
 
-// Active Links
-
-const navLinksEls = document.querySelectorAll(".navbar-links");
-
-navLinksEls.forEach((navLinkEl) => {
-  navLinkEl.addEventListener("click", () => {
-    document.querySelector(".active")?.classList.remove("active");
-    navLinkEl.classList.add("active");
-  });
-});
-
-// Testimonial Swiper
+// Swiper JS Code
 
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
   spaceBetween: 30,
-  grabCursor: "true",
+  grabCursor: true,
   autoplay: {
     delay: 2500,
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    dynamicBullets: "true",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    1024: {
+      slidesPerView: 2,
+    },
   },
 });
 
@@ -54,42 +53,4 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
-});
-
-// Smooth Scrolling
-
-const headerSection = document.querySelector(".header");
-
-document.querySelector(".to-top-link").addEventListener("click", () => {
-  headerSection.scrollIntoView({ behavior: "smooth" });
-});
-
-const workSection = document.querySelector(".work-section");
-
-document.querySelector(".work-link").addEventListener("click", () => {
-  workSection.scrollIntoView({ behavior: "smooth" });
-});
-
-const clientSection = document.querySelector(".client-section");
-
-document.querySelector(".client-link").addEventListener("click", () => {
-  clientSection.scrollIntoView({ behavior: "smooth" });
-});
-
-const aboutSection = document.querySelector(".about-section");
-
-document.querySelector(".about-link").addEventListener("click", () => {
-  aboutSection.scrollIntoView({ behavior: "smooth" });
-});
-
-var contactSection = document.querySelector(".contact-section");
-
-document.querySelector(".contact-link").addEventListener("click", () => {
-  contactSection.scrollIntoView({ behavior: "smooth" });
-});
-
-var contactSection = document.querySelector(".contact-section");
-
-document.querySelector(".help-btn-link").addEventListener("click", () => {
-  contactSection.scrollIntoView({ behavior: "smooth" });
 });
