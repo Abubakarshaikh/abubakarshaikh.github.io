d01d631832d6e2fe25943d7fdbd690f60c21cb33
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2023-04-18 20:26:59 +0500

    update again

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-// Scroll Animation
-
-window.addEventListener("scroll", reveal);
-
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windoheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
-
-    if (revealtop < windoheight - revealpoint) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-// Active Links
-
-const navLinksEls = document.querySelectorAll(".navbar-links");
-
-navLinksEls.forEach((navLinkEl) => {
-  navLinkEl.addEventListener("click", () => {
-    document.querySelector(".active")?.classList.remove("active");
-    navLinkEl.classList.add("active");
-  });
-});
-
-// Testimonial Swiper
-
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  grabCursor: "true",
-  autoplay: {
-    delay: 2500,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: "true",
-  },
-});
-
 // To Top Button
 
 const toTop = document.querySelector(".to-top");
@@ -58,10 +12,20 @@ window.addEventListener("scroll", () => {
 
 // Smooth Scrolling
 
-const headerSection = document.querySelector(".header");
+const headerSection = document.querySelector("header");
 
 document.querySelector(".to-top-link").addEventListener("click", () => {
   headerSection.scrollIntoView({ behavior: "smooth" });
+});
+
+const contactSection = document.querySelector(".contact-section");
+
+document.querySelector(".btn-link").addEventListener("click", () => {
+  contactSection.scrollIntoView({ behavior: "smooth" });
+});
+
+document.querySelector(".contact-link").addEventListener("click", () => {
+  contactSection.scrollIntoView({ behavior: "smooth" });
 });
 
 const workSection = document.querySelector(".work-section");
@@ -70,10 +34,10 @@ document.querySelector(".work-link").addEventListener("click", () => {
   workSection.scrollIntoView({ behavior: "smooth" });
 });
 
-const clientSection = document.querySelector(".client-section");
+const clientsSection = document.querySelector(".clients-section");
 
 document.querySelector(".client-link").addEventListener("click", () => {
-  clientSection.scrollIntoView({ behavior: "smooth" });
+  clientsSection.scrollIntoView({ behavior: "smooth" });
 });
 
 const aboutSection = document.querySelector(".about-section");
@@ -82,14 +46,22 @@ document.querySelector(".about-link").addEventListener("click", () => {
   aboutSection.scrollIntoView({ behavior: "smooth" });
 });
 
-var contactSection = document.querySelector(".contact-section");
+// Image Scroll
 
-document.querySelector(".contact-link").addEventListener("click", () => {
-  contactSection.scrollIntoView({ behavior: "smooth" });
-});
+window.addEventListener("scroll", reveal);
 
-var contactSection = document.querySelector(".contact-section");
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-document.querySelector(".help-btn-link").addEventListener("click", () => {
-  contactSection.scrollIntoView({ behavior: "smooth" });
-});
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
